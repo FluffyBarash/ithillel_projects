4f0e785d2f985dc8e93c3d649c4eacf2a638b168
FluffyBarash <paramonov.3022@gmail.com>
2023-11-17 10:58:08 +0200

    HomeWork_9

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector(".menu__button");
-const button_windows = document.querySelector(".button_windows");
-
-button.addEventListener("click", () => {
-  button_windows.classList.toggle("button_on");
-  button.classList.toggle("button_activated");
-});
